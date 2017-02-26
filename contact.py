89187b5f3c885022b9b3b67e19b5214605b70e5c
beorlow <be.orlow@gmail.com>
2017-02-26 22:04:17 +0100

    wyodrębniono pomocnicze metody i klasy

---
+++ contact.py
@@ -0,0 +1,25 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, homephone,
+                       mobilephone, workphone, faxnumber, email1, email2, email3, website, birthyear, annivyear,
+                       address2, homephone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.faxnumber = faxnumber
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.website = website
+        self.birthyear = birthyear
+        self.annivyear = annivyear
+        self.address2 = address2
+        self.homephone2 = homephone2
+        self.notes = notes
