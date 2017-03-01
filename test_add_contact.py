5d0cd48bc97ff481d105c09e39c51ff0757feca5
beorlow <be.orlow@gmail.com>
2017-03-01 20:28:49 +0100

    Testy add_group i add_contact przeniesione na py.test, zrealizowane fikstury

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,135 +1,33 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from contact import Contact
-
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+from application_2 import Application2
 
 
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, Contact(firstname="Emme", middlename="Anne", lastname="Imme", nickname="Onne", title="Engineer",
-                            company="Engineering", address="Doscoe 6 Wisconsin", homephone="111 111 111",
-                            mobilephone="222 222 222", workphone="333 333 333", faxnumber="546 789 876",
-                            email1="kooll@anne.com", email2="loook@mo.com", email3="inne@mol.com",
-                            website="zaggfa.co.uk", address2="Selby 72",
-                            homephone2="555 555 555", notes="Chief Engineer"))
-        self.return_to_home_contact_page(wd)
-        self.logout(wd)
+@pytest.fixture
+def app2(request):
+    fixture = Application2()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def test_add__empty_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, Contact(firstname="", middlename="", lastname="", nickname="", title="",
-                            company="", address="", homephone="",
-                            mobilephone="", workphone="", faxnumber="",
-                            email1="", email2="", email3="",
-                            website="", address2="",
-                            homephone2="", notes=""))
-        self.return_to_home_contact_page(wd)
-        self.logout(wd)
+def test_add_contact(app2):
+    app2.login(username="admin", password="secret")
+    app2.create_contact(Contact(firstname="Emme", middlename="Anne", lastname="Imme", nickname="Onne", title="Engineer",
+                        company="Engineering", address="Doscoe 6 Wisconsin", homephone="111 111 111",
+                        mobilephone="222 222 222", workphone="333 333 333", faxnumber="546 789 876",
+                        email1="kooll@anne.com", email2="loook@mo.com", email3="inne@mol.com",
+                        website="zaggfa.co.uk", address2="Selby 72",
+                        homephone2="555 555 555", notes="Chief Engineer"))
+    app2.logout()
 
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-
-    def return_to_home_contact_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-
-    def create_contact(self, wd, contact):
-        # init contact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill out contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.homephone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.workphone)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.faxnumber)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.website)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.homephone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        # submit contact creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost:8080/addressbook/")
-
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add__empty_contact(app2):
+    app2.login(username="admin", password="secret")
+    app2.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="",
+                        company="", address="", homephone="",
+                        mobilephone="", workphone="", faxnumber="",
+                        email1="", email2="", email3="",
+                        website="", address2="",
+                        homephone2="", notes=""))
+    app2.logout()
