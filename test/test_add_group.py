c3150d7e74b81286455a71cd59e21b5d04975c9e
beorlow <be.orlow@gmail.com>
2017-03-08 19:09:36 +0100

    Wyodrębniona klasa do pracy z grupami

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,10 +14,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="dfsdfdfsf", header="dsfsdf", footer="sdfsdf"))
+    app.group.create(Group(name="dfsdfdfsf", header="dsfsdf", footer="sdfsdf"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
