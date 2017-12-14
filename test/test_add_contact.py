20fdcbbb1b53f3361ff68c9a0c058e664d726723
beorlow <be.orlow@gmail.com>
2017-12-14 20:43:09 +0100

    Moved login and logout to conftest.py (one browser session)
    All tests run in one browser session.
    At this point it only works if all tests pass without interruptions.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,21 +4,19 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="Emme", middlename="Anne", lastname="Imme", nickname="Onne", title="Engineer",
                         company="Engineering", address="Doscoe 6 Wisconsin", home="111 111 111",
                         mobile="222 222 222", work="333 333 333", fax="546 789 876",
                         email="kooll@anne.com", email2="loook@mo.com", email3="inne@mol.com",
                         homepage="zaggfa.co.uk", address2="Selby 72", phone2="555 555 555", notes="Chief Engineer"))
-    app.session.logout()
+
 
 
 def test_add__empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="",
                         company="", address="", home="",
                         mobile="", work="", fax="",
                         email="", email2="", email3="",
                         homepage="", address2="",
                         phone2="", notes=""))
-    app.session.logout()
+
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,12 +1,8 @@
 from model.contact import Contact
 
 def test_modify_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="New Firstname"))
-    app.session.logout()
 
 
 def test_modify_contact_lastname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(lastname="New Lastname"))
-    app.session.logout()
