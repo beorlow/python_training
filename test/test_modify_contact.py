1769698a1cab9f2f7281d6f322037a3d5dca6d2d
beorlow <be.orlow@gmail.com>
2018-01-15 19:58:05 +0100

    Zapewniono wykonywanie warunków wstępnych w testach modyfikacji grup i kontaktów

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,8 +1,12 @@
 from model.contact import Contact
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create_contact(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="New Firstname"))
 
 
 def test_modify_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create_contact(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(lastname="New Lastname"))
